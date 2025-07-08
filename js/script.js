ec154efc61fac91a2ea14c537ec1402fbf93dd1e
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-08 15:44:25 +0200

    fix: simplify the verification of the reversed word and the inserted one

--- js/script.js
+++ js/script.js
@@ -5,13 +5,9 @@ const userWord = prompt(`Inserisci una parola`);
 //creazione di una funzione che verifica se la parola inserita è palindroma
 function isPalindrome(word) {
   //creazione di una variabile che contiene la parola al contrario
-  const reversedWord = word.split('').reverse().join('');
+  const reversedWord = word.split('').reverse().join('').toLowerCase();
   //verifica se la parola inserita è uguale a quella al contrario
-  if (word === reversedWord) {
-    return true;
-  } else {
-    return false;
-  }
+  return word === reversedWord;
 }
 //creazione di una variabile che invoca e contiene il risultato della funzione
 const result = isPalindrome(userWord);
